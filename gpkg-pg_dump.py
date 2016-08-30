b3c7f7178628281d9480c3b1c96f8ada9f0c9db6
Stephan Meißl <stephan@meissl.name>
2016-08-30 18:01:00 +0200

    Adding spatial subsetting to dump script. Uses only highest zoom level for now.

--- gpkg-pg_dump.py
+++ gpkg-pg_dump.py
@@ -65,9 +65,11 @@ def create_gpkg(
         )
 
         proj = osr.SpatialReference()
-        proj.SetWellKnownGeogCS(proj_string)
+        res = proj.SetWellKnownGeogCS(proj_string)
+        if res != 0:
+            if proj_string[0:4] == 'EPSG':
+                proj.ImportFromEPSG(int(proj_string[5:]))
         gpkg.SetProjection(proj.ExportToWkt())
-
         gpkg.SetGeoTransform(geotransform)
         gpkg = None
     except Exception as e:
@@ -81,7 +83,6 @@ def create_gpkg(
 def record_to_string(record):
     type_str = type('str')
     type_datetime = type(datetime.datetime.now())
-    type_buffer = type(buffer(""))
     type_None = type(None)
 
     values = []
@@ -95,15 +96,12 @@ def record_to_string(record):
             values.append(
                 '"' + utc.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z"'
             )
-        #Binary data is handled separately
-        elif type(item) == type_buffer:
-            values.append("''")
         else:
             values.append(str(item))
     return ','.join(values)
 
 
-def copy_table(conn_in, conn_out, table_name, constraint=None, has_blob=False):
+def copy_table(conn_in, conn_out, table_name, constraint=None):
     with conn_in.cursor() as cursor_in:
         #Check that table exists
         cursor_in.execute(
@@ -125,13 +123,6 @@ def copy_table(conn_in, conn_out, table_name, constraint=None, has_blob=False):
                         "INSERT INTO \"%s\" VALUES (%s);" %
                         (table_name, values)
                     )
-                    #If binary data present:
-                    if has_blob:
-                        cursor_out.execute(
-                            "UPDATE \"%s\" SET tile_data = ? WHERE id = %s;" %
-                            (table_name, record[0]),
-                            (sqlite3.Binary(str(record[4])),)
-                        )
                 except Exception as e:
                     conn_out.rollback()
                     sys.stderr.write(
@@ -158,11 +149,12 @@ def dump_gpkg(pg_connection_string, gpkg_name, srcwin=None):
 
             #get projection, geotransform, and size
             cursor_in.execute(
-                "SELECT srs.organization, srs.srs_id, con.min_x, "
-                "ma.pixel_x_size, 0, con.max_y, 0, -ma.pixel_y_size, "
-                "(con.max_x-con.min_x)/ma.pixel_x_size, "
+                "SELECT srs.organization, srs.organization_coordsys_id, "
+                "con.min_x, ma.pixel_x_size, 0, con.max_y, 0, "
+                "-ma.pixel_y_size, (con.max_x-con.min_x)/ma.pixel_x_size, "
                 "(con.max_y-con.min_y)/ma.pixel_y_size, con.identifier, "
-                "con.description FROM gpkg_contents con, gpkg_spatial_ref_sys "
+                "con.description, ma.matrix_width, ma.matrix_height, "
+                "ma.zoom_level FROM gpkg_contents con, gpkg_spatial_ref_sys "
                 "srs, gpkg_tile_matrix ma, (SELECT max(zoom_level) as max "
                 "FROM gpkg_tile_matrix) max WHERE con.table_name = '%s' AND "
                 "con.srs_id = srs.srs_id AND ma.table_name = con.table_name "
@@ -177,25 +169,105 @@ def dump_gpkg(pg_connection_string, gpkg_name, srcwin=None):
                 creation_options.append('RASTER_IDENTIFIER=%s' % result[10])
             if result[11] is not None and result[11] != "":
                 creation_options.append('RASTER_DESCRIPTION=%s' % result[11])
+            matrix_size = result[12:14]
+            max_zoom_level = result[14]
 
-            #TODO apply srcwin
+            #apply srcwin
+            if srcwin is not None:
+                #check srcwin
+                if srcwin[0] < 0 or srcwin[1] < 0 or \
+                   srcwin[2] < 1 or srcwin[3] < 1 or \
+                   (srcwin[0]+srcwin[2]) > matrix_size[0] or \
+                   (srcwin[1]+srcwin[3]) > matrix_size[1]:
+                    sys.stderr.write(
+                        "ERROR: Invalid srcwin %s. First and second values "
+                        "cannot be less than 0, third and forth values cannot "
+                        "be less than 1, sum of first and third value cannot "
+                        "be more than %s and sum of second and forth value "
+                        "cannot be more than %s.\n"
+                        % (srcwin, matrix_size[0], matrix_size[1])
+                    )
+                    sys.exit(1)
 
-            #tables gpkg_contents,gpkg_spatial_ref_sys, gpkg_tile_matrix_set,
+                if (srcwin[0]+srcwin[2]) != matrix_size[0]:
+                    size[0] = srcwin[2]*256
+                else:
+                    size[0] = size[0]-(srcwin[0]*256)
+                if (srcwin[1]+srcwin[3]) != matrix_size[1]:
+                    size[1] = srcwin[3]*256
+                else:
+                    size[1] = size[1]-(srcwin[1]*256)
+                tmp = geotransform
+                geotransform = []
+                geotransform.append(tmp[0]+tmp[1]*256*srcwin[0])
+                geotransform.append(tmp[1])
+                geotransform.append(tmp[2])
+                geotransform.append(tmp[3]+tmp[5]*256*srcwin[1])
+                geotransform.append(tmp[4])
+                geotransform.append(tmp[5])
+
+                constraint = (
+                    "zoom_level = %i AND tile_column >= %i AND "
+                    "tile_column < %i AND tile_row >= %i AND tile_row < %i "
+                    "ORDER BY id"
+                    % (max_zoom_level, srcwin[0], srcwin[0]+srcwin[2],
+                       srcwin[1], srcwin[1]+srcwin[3])
+                )
+
+            else:
+                srcwin = [0, 0, matrix_size[0], matrix_size[1]]
+                constraint = None
+
+            #tables gpkg_contents, gpkg_spatial_ref_sys, gpkg_tile_matrix_set,
             #and gpkg_tile_matrix are handled by GDAL
             create_gpkg(
                 gpkg_name, proj_string, size, geotransform, creation_options
             )
 
-        with sqlite3.connect("%s.gpkg" % gpkg_name) as conn_out:
-            #dump metadata
-            copy_table(conn_in, conn_out, "gpkg_metadata_reference",
-                       "table_name = '%s'" % gpkg_name)
-            copy_table(conn_in, conn_out, "gpkg_metadata",
-                       "id IN (SELECT md_file_id FROM gpkg_metadata_reference "
-                       "WHERE table_name = '%s')" % gpkg_name)
+            with sqlite3.connect("%s.gpkg" % gpkg_name) as conn_out:
+                #dump metadata
+                copy_table(conn_in, conn_out, "gpkg_metadata_reference",
+                           "table_name = '%s'" % gpkg_name)
+                copy_table(conn_in, conn_out, "gpkg_metadata",
+                           "id IN (SELECT md_file_id FROM "
+                           "gpkg_metadata_reference WHERE table_name = '%s')"
+                           % gpkg_name)
 
-            #dump tiles
-            copy_table(conn_in, conn_out, gpkg_name, has_blob=True)
+                #dump tiles
+                #copy_table(conn_in, conn_out, gpkg_name, has_blob=True)
+
+                cursor_in.execute(
+                    "SELECT id, zoom_level, tile_column, tile_row, tile_data "
+                    "FROM \"%s\"%s;" % (gpkg_name, "" if constraint is
+                                        None else " WHERE " + constraint)
+                )
+                records = cursor_in.fetchall()
+
+                cursor_out = conn_out.cursor()
+                cursor_out.execute(
+                    "SELECT max(zoom_level) FROM gpkg_tile_matrix WHERE "
+                    "table_name = '%s';" % gpkg_name
+                )
+                zoom_offset = max_zoom_level - cursor_out.fetchone()[0]
+                for record in records:
+
+                    try:
+                        cursor_out.execute(
+                            "INSERT INTO \"%s\" (zoom_level, tile_column, "
+                            "tile_row, tile_data) VALUES (?, ?, ?, ?);"
+                            % gpkg_name,
+                            (record[1]-zoom_offset, record[2]-srcwin[0],
+                             record[3]-srcwin[1],
+                             sqlite3.Binary(str(record[4])))
+                        )
+                    except Exception as e:
+                        conn_out.rollback()
+                        sys.stderr.write(
+                            "ERROR: Input doesn't seem to be a valid "
+                            "GeoPackage. Error message was: '%s'.\n"
+                            % e.message
+                        )
+                        sys.exit(1)
 
 
 def main():
@@ -216,17 +288,12 @@ def main():
     )
     parser.add_argument(
         "-srcwin", nargs=4, type=int,
+        metavar=("xoff", "yoff", "xsize", "ysize"),
         help="Selects a subwindow from the source GeoPackage for dumping "
         "based on tile indexes starting from 0 0 at the top left."
     )
 
     args = parser.parse_args()
-    if args.srcwin[2] < args.srcwin[0] or args.srcwin[3] < args.srcwin[1]:
-        sys.stderr.write(
-            "ERROR: Invalid srcwin %s. Third value cannot be smaller than "
-            "first value and forth than second.\n" % args.srcwin
-        )
-        sys.exit(1)
 
     dump_gpkg(args.pg_connection_string, args.gpkg_name, args.srcwin)
 
