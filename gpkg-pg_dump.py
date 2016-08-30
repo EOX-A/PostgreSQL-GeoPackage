5aa191ac1ac029c41f4abfafc27c37e0b3dc8589
Stephan Meißl <stephan@meissl.name>
2016-08-30 15:12:13 +0200

    Add proper arguments parsing and adjust SQLite GeoPackage creation via GDAL.

--- gpkg-pg_dump.py
+++ gpkg-pg_dump.py
@@ -38,13 +38,18 @@
 
 import sys
 import os
+import argparse
 import sqlite3
 import datetime
 import psycopg2
 from osgeo import gdal
+from osgeo import osr
 
 
-def create_empyt_gpkg(gpkg_name):
+def create_gpkg(
+    gpkg_name, proj_string, size=(1, 1), geotransform=[0, 1, 0, 0, 0, -1],
+    creation_options=None
+):
     if os.path.exists("%s.gpkg" % gpkg_name):
         sys.stderr.write(
             "ERROR: SQLite GeoPackage '%s.gpkg' already exists.\n" % gpkg_name
@@ -54,7 +59,16 @@ def create_empyt_gpkg(gpkg_name):
     gdal.AllRegister()
     drv = gdal.GetDriverByName("GPKG")
     try:
-        gpkg = drv.Create("%s.gpkg" % gpkg_name, 0, 0, 1, gdal.GDT_Byte)
+        gpkg = drv.Create(
+            "%s.gpkg" % gpkg_name, size[0], size[1], 1, gdal.GDT_Byte,
+            creation_options
+        )
+
+        proj = osr.SpatialReference()
+        proj.SetWellKnownGeogCS(proj_string)
+        gpkg.SetProjection(proj.ExportToWkt())
+
+        gpkg.SetGeoTransform(geotransform)
         gpkg = None
     except Exception as e:
         sys.stderr.write(
@@ -127,55 +141,100 @@ def copy_table(conn_in, conn_out, table_name, constraint=None, has_blob=False):
                     sys.exit(1)
 
 
-def dump_gpkg(pg_connection_string, gpkg_name):
+def dump_gpkg(pg_connection_string, gpkg_name, srcwin=None):
     with psycopg2.connect(pg_connection_string) as conn_in:
-        with sqlite3.connect("%s.gpkg" % gpkg_name) as conn_out:
-            #Check that GeoPackage exists
-            with conn_in.cursor() as cursor_in:
-                cursor_in.execute(
-                    "SELECT to_regclass('\"%s\"');"
-                    % gpkg_name
+        #Check that GeoPackage exists
+        with conn_in.cursor() as cursor_in:
+            cursor_in.execute(
+                "SELECT to_regclass('\"%s\"');"
+                % gpkg_name
+            )
+            if cursor_in.fetchone()[0] is None:
+                sys.stderr.write(
+                    "ERROR: GeoPackage '%s' not found in PostgreSQL.\n" %
+                    gpkg_name
                 )
-                if cursor_in.fetchone()[0] is None:
-                    sys.stderr.write(
-                        "ERROR: GeoPackage '%s' not found in PostgreSQL.\n" %
-                        gpkg_name
-                    )
-                    sys.exit(1)
+                sys.exit(1)
 
-            copy_table(conn_in, conn_out, "gpkg_spatial_ref_sys",
-                       "srs_id NOT IN ('-1','0','4326')")
-            copy_table(conn_in, conn_out, "gpkg_contents",
-                       "table_name = '%s'" % gpkg_name)
-            copy_table(conn_in, conn_out, "gpkg_tile_matrix_set",
-                       "table_name = '%s'" % gpkg_name)
-            copy_table(conn_in, conn_out, "gpkg_tile_matrix",
-                       "table_name = '%s'" % gpkg_name)
+            #get projection, geotransform, and size
+            cursor_in.execute(
+                "SELECT srs.organization, srs.srs_id, con.min_x, "
+                "ma.pixel_x_size, 0, con.max_y, 0, -ma.pixel_y_size, "
+                "(con.max_x-con.min_x)/ma.pixel_x_size, "
+                "(con.max_y-con.min_y)/ma.pixel_y_size, con.identifier, "
+                "con.description FROM gpkg_contents con, gpkg_spatial_ref_sys "
+                "srs, gpkg_tile_matrix ma, (SELECT max(zoom_level) as max "
+                "FROM gpkg_tile_matrix) max WHERE con.table_name = '%s' AND "
+                "con.srs_id = srs.srs_id AND ma.table_name = con.table_name "
+                "AND ma.zoom_level = max.max;" % gpkg_name
+            )
+            result = cursor_in.fetchone()
+            proj_string = "%s:%i" % result[0:2]
+            geotransform = result[2:8]
+            size = [int(i) for i in result[8:10]]
+            creation_options = []
+            if result[10] is not None and result[10] != "":
+                creation_options.append('RASTER_IDENTIFIER=%s' % result[10])
+            if result[11] is not None and result[11] != "":
+                creation_options.append('RASTER_DESCRIPTION=%s' % result[11])
+
+            #TODO apply srcwin
+
+            #tables gpkg_contents,gpkg_spatial_ref_sys, gpkg_tile_matrix_set,
+            #and gpkg_tile_matrix are handled by GDAL
+            create_gpkg(
+                gpkg_name, proj_string, size, geotransform, creation_options
+            )
+
+        with sqlite3.connect("%s.gpkg" % gpkg_name) as conn_out:
+            #dump metadata
             copy_table(conn_in, conn_out, "gpkg_metadata_reference",
                        "table_name = '%s'" % gpkg_name)
             copy_table(conn_in, conn_out, "gpkg_metadata",
                        "id IN (SELECT md_file_id FROM gpkg_metadata_reference "
                        "WHERE table_name = '%s')" % gpkg_name)
+
+            #dump tiles
             copy_table(conn_in, conn_out, gpkg_name, has_blob=True)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) <= 2:
+def main():
+    parser = argparse.ArgumentParser(
+        description="This script dumps a PostgreSQL-GeoPackage database into "
+        "a SQLite GeoPackage."
+    )
+    parser.add_argument(
+        "pg_connection_string",
+        help="Connection string for PostgreSQL e.g. \"dbname='gpkg' "
+        "user='gpkg'\"."
+    )
+    parser.add_argument(
+        "gpkg_name",
+        help="The GeoPackage name used to determine the table in which the "
+        "tile data is stored as well as for the filename of the SQLite "
+        "GeoPackage to generate."
+    )
+    parser.add_argument(
+        "-srcwin", nargs=4, type=int,
+        help="Selects a subwindow from the source GeoPackage for dumping "
+        "based on tile indexes starting from 0 0 at the top left."
+    )
+
+    args = parser.parse_args()
+    if args.srcwin[2] < args.srcwin[0] or args.srcwin[3] < args.srcwin[1]:
         sys.stderr.write(
-            "ERROR: Please provide the connection string for PostgreSQL as "
-            "well as the GeoPackage name. The GeoPackage name is used to "
-            "determine the table in which the tile data is stored as well as "
-            "for the filename of the SQLite GeoPackage to generate.\n"
+            "ERROR: Invalid srcwin %s. Third value cannot be smaller than "
+            "first value and forth than second.\n" % args.srcwin
         )
         sys.exit(1)
 
-    pg_connection_string = sys.argv[1]
-    gpkg_name = sys.argv[2]
-
-    create_empyt_gpkg(gpkg_name)
-    dump_gpkg(pg_connection_string, gpkg_name)
+    dump_gpkg(args.pg_connection_string, args.gpkg_name, args.srcwin)
 
     sys.stdout.write(
-        "GeoPackage '%s' successfully exported\n" % gpkg_name
+        "GeoPackage '%s' successfully exported\n" % args.gpkg_name
     )
     sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
