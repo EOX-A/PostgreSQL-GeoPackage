ad7ff30d3058799d5563c54824796e772d610f53
Stephan Meißl <stephan@meissl.name>
2016-10-21 13:36:06 +0200

    Adding PostgreSQL and related packages.

--- vagrant/scripts/packages.sh
+++ vagrant/scripts/packages.sh
@@ -2,4 +2,4 @@
 
 echo "Package installation provision step"
 
-aptitude install -y gdal-bin python-gdal postgis python-psycopg2 sqlite3
+aptitude install -y gdal-bin python-gdal postgis python-psycopg2 sqlite3 postgresql postgresql-common postgresql-client-common postgresql-9.5-postgis-2.2 postgresql-9.5-postgis-scripts
